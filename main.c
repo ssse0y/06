b9415ea7cdbc417570e7b9fcf78c48df91f407ee
ssse0y <ko527ko@gmail.com>
2022-10-11 17:04:46 +0900

    #4

--- main.c
+++ main.c
@@ -1,28 +1,15 @@
 #include <stdio.h>
 
-int sumTwo(int a, int b){
-   int op;
-   op=a+b;
-   return op;
+int square(int a)
+{
+	return(a*a);
 }
 
-int square(int n){
-   return(n*n);
+int main()
+{
+	int a=2;
+	a=square(a);
+	printf("a=%i\n", a);
 }
 
-int get_max(int x, int y){
-   if (x>y) return(x);
-   else return(y);
-}
 
-int main(void){
-   int sum, sq, max;
-   sum= sumTwo(2,5);
-   sq=square(3);
-   max=get_max(6,5);
-   printf("두 숫자의 합: %d\n", sum);
-   printf("정수의 제곱: %d\n", sq);
-   printf("큰 수 구하기: %d\n", max);
-   
-   return 0;
-}
