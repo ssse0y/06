9b6d2e03d50b0f48a4facc9bbf45ce011dd2b001
ssse0y <ko527ko@gmail.com>
2022-10-11 17:00:51 +0900

    #3

--- main.c
+++ main.c
@@ -1,14 +1,28 @@
 #include <stdio.h>
 
-void func1(void){
-	int x;
-	printf("func1 x is at %p\n",&x);
+int sumTwo(int a, int b){
+   int op;
+   op=a+b;
+   return op;
+}
+
+int square(int n){
+   return(n*n);
+}
+
+int get_max(int x, int y){
+   if (x>y) return(x);
+   else return(y);
 }
 
 int main(void){
-	int x; 
-	printf("main x is at %p\n",&x);
-	func1();
-	
-	return 0;
+   int sum, sq, max;
+   sum= sumTwo(2,5);
+   sq=square(3);
+   max=get_max(6,5);
+   printf("두 숫자의 합: %d\n", sum);
+   printf("정수의 제곱: %d\n", sq);
+   printf("큰 수 구하기: %d\n", max);
+   
+   return 0;
 }
