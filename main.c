02e345b30eeec9bc9622175714507d8fa4cd4ab3
ssse0y <ko527ko@gmail.com>
2022-10-12 13:49:39 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,15 +1,34 @@
 #include <stdio.h>
 
-int square(int a)
+int comb(int x, int y, int z) 
 {
-	return(a*a);
+	int res1=x/(y*z);
+	return res1;
 }
 
-int main()
+int fac(int n)
 {
-	int a=2;
-	a=square(a);
-	printf("a=%i\n", a);
+	int res=1;
+	int i;
+	
+	for(i=1 ; i<=n ; i++)
+	   res=res*i;
+	
+	return res;
 }
 
+int main(void) {
+	int a, b, n1, n2, n3, n4;
+	printf("n,r 값을 순서대로 입력하세요:");
+	scanf("%d %d", &a, &b);
+	n1=fac(a);
+	n2=fac(a-b);
+	n3=fac(b);
+	n4=comb(n1, n2, n3);
+	printf("combination의 결과: %d \n", n4);
+	return 0;
+	
+	
+}
 
+	
